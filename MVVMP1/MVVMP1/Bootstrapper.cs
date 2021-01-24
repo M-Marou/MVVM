0697832ed83ec9625cf98e6d87fd19ed7e90fdf9
Marouane Moumni <MAR@YC.com>
2021-01-24 17:00:42 +0200

    added MVVM

---
+++ MVVMP1/MVVMP1/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using MVVMP1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMP1
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper(){
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ MVVMP1/MVVMP1/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMP1.ViewModels
+{
+    class ShellViewModel : Screen
+    {
+        private string _firstName = "MarOne";
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; NotifyOfPropertyChange(() => FirstName); }
+        }
+
+    }
+}
