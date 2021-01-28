3433df4068c3719c551e4d536f066a2e2ad866a8
Marouane Moumni <57830533+M-Marou@users.noreply.github.com>
2021-01-28 01:10:25 +0100

    Added sql connection

--- DGV/DGV/ViewModels/DisplayViewModel.cs
+++ DGV/DGV/ViewModels/DisplayViewModel.cs
@@ -5,11 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using System.Configuration;
+using System.Data;
 
 namespace DGV.ViewModels
 {
     public class DisplayViewModel : Screen
     {
-        
+        public void DataBinding()
+        {
+            SqlConnection con = new SqlConnection();
+            String SqlConnection = ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM [Employee]";
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("Employee");
+            da.Fill(dt);
+
+            myDG.ItemSource = dt.DefaultView;
+        }
     }
 }
